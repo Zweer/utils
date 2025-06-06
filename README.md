2a4039d2db8a715c12fb0b834ec85c359676794c
Zweer <n.olivieriachille@gmail.com>
2025-06-06 19:04:18 +0200

    build: :technologist: Added `eslint`, `husky`, `vitest` and `tsconfig`

---
+++ README.md
@@ -0,0 +1,5 @@
+# zweer/utils
+
+![Coverage Badge](https://img.shields.io/badge/coverage-91%25-yellow?style=flat)
+
+Many utilities and scripts to enhance your development experience
---
+++ eslint.config.mjs
@@ -0,0 +1,32 @@
+import antfu from '@antfu/eslint-config';
+
+export default antfu({
+  react: true,
+  stylistic: {
+    semi: true,
+    overrides: {
+      'style/brace-style': ['error', '1tbs'],
+      'no-console': 'off',
+    },
+  },
+  typescript: {
+    tsconfigPath: 'tsconfig.json',
+  },
+}, {
+  rules: {
+    'perfectionist/sort-imports': ['error', {
+      internalPattern: ['^~/.+', '^@/.+', '^#.+'],
+      groups: [
+        'type',
+        ['parent-type', 'sibling-type', 'index-type', 'internal-type'],
+        'builtin',
+        'external',
+        'internal',
+        ['parent', 'sibling', 'index'],
+        'side-effect',
+        'object',
+        'unknown',
+      ],
+    }],
+  },
+});
